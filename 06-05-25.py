58b8bb9d2b0458c7181bb48bc5dd377468fa7d8f
thur-7-7 <vinicioarthur690@gmail.com>
2025-05-06 17:39:59 -0300

    atual

--- 06-05-25.py
+++ 06-05-25.py
@@ -2,9 +2,13 @@ import sys
 
 Vo = int(input('vel inicial:'))
 if Vo <=0:
-  sys.exit('informe velocidade positiva')
+    sys.exit('informe velocidade positiva')
 tempo = int(input('tempo:'))
+if tempo <=0:
+    sys.exit('informe um intervalo de tempo positivo')
 acel = int(input('aceleração:'))
+if acel <=0:
+    sys.exit('informe uma aceleração positiva')
 
 dist = Vo * tempo + ((acel * (tempo**2)) / 2)
 
