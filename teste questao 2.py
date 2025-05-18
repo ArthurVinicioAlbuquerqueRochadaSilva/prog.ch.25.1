703b4d0be48c7b1983df591427f9f92efc7718f7
ArthurVinicioAlbuquerqueRochadaSilva <vinicioarthur690@gmail.com>
2025-05-18 18:22:22 -0300

    atual q2

---
+++ teste questao 2.py
@@ -0,0 +1,8 @@
+print('Saque de caixa eletrônico')
+
+valor = int(input(' Valor do saque:R$'))
+total = valor
+ced = 100
+totced = valor / ced
+if total >= 100:
+    print('Total de cédulas'.totced)
