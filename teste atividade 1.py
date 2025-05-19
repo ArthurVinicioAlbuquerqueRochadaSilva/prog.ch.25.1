050228cef34e64c569d8be1d1b57a41753d40662
ArthurVinicioAlbuquerqueRochadaSilva <vinicioarthur690@gmail.com>
2025-05-18 21:34:24 -0300

    questao atualizada

--- teste atividade 1.py
+++ teste atividade 1.py
@@ -1,19 +1,23 @@
 import sys
 
-num = int(input('digite um número inteiro de até 4 digitos '))
+num = int(input('Digite um número inteiro de até 4 digitos:'))
+
 unidade = num % 10 
 dezena = (num // 10) % 10
 centena = (num // 100) % 10
 milhar = (num// 1000) % 10
 
-if num <= 0  > 9999:
-    sys.exit('ERRO: informe número inteiro positivo de até 4 dígitos')
+if num <= 0:
+    sys.exit('ERRO: Informe número inteiro positivo de até 4 digitos')
+elif num > 9999:
+     print(f'ERRO: Informe número inteiro positivo de até 4 dígitos')
 
-if num <= 9:
-    print(f'soma dos algarismos: {unidade}')
-if num >= 10 <= 99:
-    print(f'soma dos algarismos:{dezena + unidade} ')
-if num >= 100 <=999:
-    print(f'soma dos algarismos:{centena + dezena + unidade}')
-if num >= 1000 <= 9999:
-    print(f'soma dos algarismos:{milhar + centena + dezena + unidade}')
+if num <= 9999:
+    print(f'Soma dos algarismos: {milhar + centena + dezena + unidade}')
+elif num <= 999:
+    print(f'Soma dos algarismos: {centena + dezena + unidade}')
+elif num <= 99:
+    print(f'Soma dos algarismos: {dezena + unidade}')
+elif num <= 9:
+    print(f'Soma dos algarismos: {unidade}')
+
