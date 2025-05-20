049a128cfc30106fa93dcb6424ffc2501b3347ae
ArthurVinicioAlbuquerqueRochadaSilva <vinicioarthur690@gmail.com>
2025-05-19 23:19:10 -0300

    finalizada questao 4

--- teste q4.py
+++ teste q4.py
@@ -1,10 +1,22 @@
-#Questão 4
-a = int(input('Informe valor de A:'))
-b = int(input('Informe valor de B:'))
-c = int(input('Informe valor de C'))
-if A <= 0:
-    print('ERRO: A tem que ser diferente de 0')
+try:
+    a = float(input('Informe valor de A:'))
+    b = float(input('Informe valor de B:'))
+    c = float(input('Informe valor de C:'))
+except ValueError:
+    print('ERRO: Digite apenas números')
 
 delta = b**2 - 4 * a * c
+if delta > 0:
+    x1 = (-b +  delta**(1/2)) / (2 * a)
+    x2 = (-b -  delta**(1/2)) / (2 * a)
+    print(f'A equação tem duas raízes distintas:')
+    print(f'Raiz 1:{x1}')
+    print(f'Raiz 2:{x2}')
 
-x = b**2 +- raiz delta / 2 * a
+elif delta == 0:
+    x = (-b**2 + delta**(1/2)) / (2 * a)
+    print(f'A equação possui uma raiz dupla')
+    print(f'Raiz:{x}')
+
+else:
+    print(f'A equação não possui raiz')
