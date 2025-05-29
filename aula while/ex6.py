33b0f7def0911786ffffb4ff66378f6fbc6414cb
ArthurVinicioAlbuquerqueRochadaSilva <vinicioarthur690@gmail.com>
2025-05-29 17:50:14 -0300

    teste

---
+++ aula while/ex6.py
@@ -0,0 +1,19 @@
+import sys
+try:
+    num = int(input('Digite um número inteiro positivo: '))
+    if num <= 0:
+        sys.exit('Digite um número inteiro postivo')
+except ValueError:
+    sys.exit('Digite valor válido')
+
+else:
+    divisor = 2
+    contador = 0
+    while contador <= num:
+        if num % divisor == 0:
+            divisor += 1
+        contador += 1
+    if divisor == 2:
+        print('Este número é primo')
+    else:
+        print('Este número não é primo')
