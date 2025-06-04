e02af504133624f65700ae1188f766860378ab73
ArthurVinicioAlbuquerqueRochadaSilva <vinicioarthur690@gmail.com>
2025-06-04 10:38:45 -0300

    test

--- aula for/q5.py
+++ aula for/q5.py
@@ -1,8 +1,7 @@
 num = int(input('Informe valor inicial da PA: '))
 razao = int(input('Informe o valor da razão da PA: '))
 nelementos = int(input('Informe a quantidade de elementos da PA: '))
-contador = 0
-while contador <= nelementos:
-     num += razao
-     contador += 1
-print (num)
+
+for pa in range(1, nelementos):
+    print(f'{num + razao}')
+print('FIM')
