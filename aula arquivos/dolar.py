9e317058c20ec46c07d75570fa0f7e6c69a981ae
ArthurVinicioAlbuquerqueRochadaSilva <vinicioarthur690@gmail.com>
2025-07-15 17:58:57 -0300

    kkk

--- aula arquivos/dolar.py
+++ aula arquivos/dolar.py
@@ -21,7 +21,9 @@
 
       dica biblioteca statisc e datatime
 '''
-import os
+import os, sys
+from datetime import datetime
+
 strDir = os.path.dirname(__file__)
 try: 
     arqLeitura = open(f'{strDir}\\cotacao_dolar.csv','r', encoding = 'utf-8')
@@ -30,15 +32,34 @@ except FileNotFoundError:
 except Exception as erro:
     print('ERRO: {erro}')
 else:
-    lstLinha = list()
     lstCabecalho = arqLeitura.readline().strip().split(';')
+    lstLinha = list()
+    lstAux = list()
+    lstCotacoes = list()
     while True:
         strLinha = arqLeitura.read().strip()
         if not strLinha: break
-        for i in strLinha:
-            strLinha.split(';')
-        lstLinha.append(strLinha)
+        
+        lstAux = strLinha.split(';')
+
+        try:
+            lstAux.append(strLinha)
+            compra = float(lstAux[0].replace(',', '.'))
+            venda = float(lstAux[1].replace(',', '.'))
+            data = datetime.strptime(lstAux[2], '%d/%m/%Y').date()
+            
+            lstCotacoes.append([compra, venda, data])
+        except Exception as e:
+            sys.exit(f'ERRO: {e}')
+
+arqLeitura.close()
+
+if lstCotacoes:
+    print("\n--- Cotações do Dolar processadas --- ")
+    for cotacao in lstCotacoes:
+        print(f"Compra: {cotacao[0]: .3f} | Data: {cotacao[2].strftime('%d/%m/%Y')}")
+    
     
 
-    print(lstCabecalho)
-    print(lstLinha)
+
+    
