1b45372564408db7f13e7d0f527fedde314429ca
ArthurVinicioAlbuquerqueRochadaSilva <vinicioarthur690@gmail.com>
2025-07-29 17:34:25 -0300

    aukallalala

---
+++ aula funções/ex1.py
@@ -0,0 +1,16 @@
+
+import sys, funcoes
+try:
+    intE1 = int(input('Informe E1: '))
+    intE2 = int(input('Informe E2: '))
+except ValueError:
+    sys.exit('Informe inteiro válido')
+except Exception as erro:
+    sys.exit('ERRO; {erro}')
+else:
+    try:
+        mediaFinal = funcoes.mediaIFRN(intE1, intE2)
+    except Exception as erro:
+        print(f'ERRO: {erro}')
+    else:
+         print(f'Média = {mediaFinal}')
---
+++ aula funções/funcoes.py
@@ -0,0 +1,19 @@
+from logging import exception
+
+
+def mediaIFRN(nota1: int, nota2: int)-> int:
+    
+    if not isinstance (nota1, int):
+        raise Exception ('Nota1 tem que ser inteiro')
+    if not isinstance (nota2, int):
+        raise Exception ('Nota2 tem que ser inteiro') 
+
+
+    if nota1 < 0 or nota1 > 100:
+        raise Exception ('Nota1 deve ser entre 0 e 100')
+    if nota2 < 0 or nota2 > 100:
+        raise Exception ('Nota2 deve ser entre 0 e 100')
+    
+    media = (nota1 * 2 + nota2 * 3 )/ 5
+    
+    return int(round(media, 0))
