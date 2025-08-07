c01cef66bc9513285346ef9868ca38a966e7d6e3
ArthurVinicioAlbuquerqueRochadaSilva <vinicioarthur690@gmail.com>
2025-08-07 17:48:04 -0300

    main

--- lista4/q2.py
+++ lista4/q2.py
@@ -1,33 +1,28 @@
-import os, sys, json
+import os, sys, json, requests
 from datetime import datetime
 
 strDirApp = os.path.dirname(__file__)
 strURLCartola = 'https://api.cartolafc.globo.com/atletas/mercado'
 
-dicCartola = requests.get(strURLCartola).json()
 x = input('Digite  o ano desejado para o cartola: ')
 ano_int = int(x)
 ano = datetime(ano_int, 1, 1)
 print(f'Ano válido: {ano}')
-
-
-try:
-   arqInput = open(f'strNomeArq{ano}', 'r', encoding='utf-8')
-   if ano == 2025:
-       #dicCartola = requests.get(strURLCartola).json()
-    elif ano < 2021:
-        sys.exit('Cartola só tem do ano 2021 até o atual')
-        
-   strDados = arqInput.read()
-   dictCartola = json.loads(strDados)
-   arqInput.close()
-except  FileNotFoundError:
-   sys.exit('ERRO: Arquivo não existe...')
-except json.JSONDecodeError:
-   sys.exit('ERRO: O conteúdo do arquivo não está no formato correto...')
-except Exception as erro:
-   sys.exit(f'ERRO: {erro}...')
+if ano == 2025:
+   dicCartola = requests.get(strURLCartola).json()
+elif ano < 2021:
+   sys.exit('Cartola só tem do ano 2021 até o atual')
 else:
+   try:
+      arqCartola = open(f'strNomeArq{ano}', 'r', encoding='utf-8')    
+   
+   except  FileNotFoundError:
+      sys.exit('ERRO: Arquivo não existe...')
+   except json.JSONDecodeError:
+      sys.exit('ERRO: O conteúdo do arquivo não está no formato correto...')
+   except Exception as erro:
+      sys.exit(f'ERRO: {erro}...')
+
    #lstChaves = list(dictCartola.keys())
    #print(lstChaves) # ['clubes', 'posicoes', 'status', 'atletas']
    
